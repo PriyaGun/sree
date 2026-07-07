a1799fcba2620f89b9d3669dc0933990ae126553
PriyaGun <104830348+PriyaGun@users.noreply.github.com>
2026-07-07 16:46:51 +0100

    Update DuckDuckGoHomePage.java

--- src/test/java/starter/actions/navigation/DuckDuckGoHomePage.java
+++ src/test/java/starter/actions/navigation/DuckDuckGoHomePage.java
@@ -3,7 +3,7 @@ package starter.actions.navigation;
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.core.pages.PageObject;
 
-@DefaultUrl("https://duckduckgo.com/")
+
 public class NavigateActions extends UIInteractions {
     public void toTheDuckDuckGoSearchPage() {
         openUrl("https://duckduckgo.com/");
